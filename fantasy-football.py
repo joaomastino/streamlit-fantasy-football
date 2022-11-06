712f32c29b603e25c0440885d26d19122dbd44d9
joao mastino <s.trinchero@gmail.com>
2022-11-06 07:16:46 +0100

    implemented csv download

--- fantasy-football.py
+++ fantasy-football.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
+
 
 st.title('NFL Fantasy Football Stats Explorer')
 
