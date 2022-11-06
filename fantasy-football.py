6c435c52900b8a52d0d8d1d79a53ead653a63dcf
joao mastino <s.trinchero@gmail.com>
2022-11-06 07:21:05 +0100

    csv download fix and new credits

--- fantasy-football.py
+++ fantasy-football.py
@@ -10,8 +10,9 @@ st.title('NFL Fantasy Football Stats Explorer')
 
 st.markdown("""
 This app performs simple webscraping of NFL Football fantasy players data from 1970 to 2022!
-* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
+* **Python libraries:** base64, pandas, streamlit
 * **Data source:** [pro-football-reference.com](https://www.pro-football-reference.com/).
+* **Inspired by:** [Data professor Youtube Tutorial](https://www.youtube.com/watch?v=zYSDlbr-8V8&ab_channel=DataProfessor)
 """)
 
 st.sidebar.header('Filter by year, role and team')
@@ -21,11 +22,6 @@ selected_year = st.sidebar.selectbox('Year', list(reversed(range(1970,2023))))
 @st.cache
 def load_data(year):
     url = "https://www.pro-football-reference.com/years/" + str(year) + "/fantasy.htm#fantasy"
-    # url = "https://www.pro-football-reference.com/years/1970/fantasy.htm#fantasy"
-
-    # if skill == 'rushing':
-    #     html = pd.read_html(url, header=1)
-    # else:
     html = pd.read_html(url, header=1)
     df = html[0]
     playerstats = df.drop(['Rk','Fmb','FL','2PM','2PP'], axis=1)[0:100]
@@ -52,4 +48,16 @@ st.dataframe(df_selected_team)
 # Download player stats data
 # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
 
+st.header('Players stats download')
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+    return href
+
+st.markdown(get_table_download_link(df_selected_team), unsafe_allow_html=True)
 
